aa58f1b253bd442e6d0f61419dfa7afb55513f71
CarlosMatellano <160237270+CarlosMatellano@users.noreply.github.com>
2024-04-04 12:44:45 +0200

    ProyectoFauna

---
+++ CentroRecuperacionFauna/src/main/java/Fauna/Animal.java
@@ -0,0 +1,60 @@
+package Fauna;
+
+
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alumno
+ */
+public class Animal {
+    private String especie;
+    private Double peso;
+    private String gravedad;
+    private Date fechaentrada;
+
+    public Animal(String especie, Double peso, String gravedad, Date fechaentrada) {
+        this.especie = especie;
+        this.peso = peso;
+        this.gravedad = gravedad;
+        this.fechaentrada = fechaentrada;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getGravedad() {
+        return gravedad;
+    }
+
+    public void setGravedad(String gravedad) {
+        this.gravedad = gravedad;
+    }
+
+    public Date getFechaentrada() {
+        return fechaentrada;
+    }
+
+    public void setFechaentrada(Date fechaentrada) {
+        this.fechaentrada = fechaentrada;
+    }
+    
+}
---
+++ CentroRecuperacionFauna/src/main/java/Fauna/Ave.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Fauna;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ave extends Animal{
+    
+    public Ave(String especie, Double peso, String gravedad, Date fechaentrada) {
+        super(especie, peso, gravedad, fechaentrada);
+    }
+    
+}
